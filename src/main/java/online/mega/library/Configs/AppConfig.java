ae759bf5a849a8bcfdd170fb21ed532128f37286
Yurlov <Benedictum1@mail.ru>
2017-02-27 02:28:43 +0300

    last commit

--- src/main/java/online/mega/library/Configs/AppConfig.java
+++ src/main/java/online/mega/library/Configs/AppConfig.java
@@ -149,7 +149,7 @@ public class AppConfig extends WebMvcConfigurerAdapter {
     public JavaMailSender javaMailSender(){
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
         javaMailSender.setUsername("bookshouselibrary@gmail.com");
-        javaMailSender.setPassword("qms437dvskd");
+        javaMailSender.setPassword("xxxxxxxxx");
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
