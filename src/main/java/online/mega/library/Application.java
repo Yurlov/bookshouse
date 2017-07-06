702b4212566c7583e2e36fa1a3aead8796abd0fe
Yurlov <Benedictum1@mail.ru>
2017-07-06 17:55:04 +0300

    Update Application.java

--- src/main/java/online/mega/library/Application.java
+++ src/main/java/online/mega/library/Application.java
@@ -1,15 +1,10 @@
 package online.mega.library;
 
-
-
-import online.mega.library.Entities.User;
-import online.mega.library.Services.UserService;
-import online.mega.library.Enams.UserRole;
+import online.mega.library.Utils.MyUtils;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
 
 @SpringBootApplication
 public class Application {
@@ -18,13 +13,12 @@ public class Application {
     }
 
 
-  @Bean
-    public CommandLineRunner demo(final UserService userService) {
+@Bean
+public CommandLineRunner demo() {
     return new CommandLineRunner() {
         @Override
         public void run(String... strings) throws Exception {
-           userService.addUser(new User("admin", "bcb049247ce6fab013d6f085b165e7c081ab93bd", UserRole.ADMIN));
-           userService.addUser(new User("user", "bcb049247ce6fab013d6f085b165e7c081ab93bd", UserRole.USER));
+            MyUtils.fillApp();
         }
     };
 }
