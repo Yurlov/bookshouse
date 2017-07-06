2d6054587c4663cbd0f0e2ef06d0aa3f3f2e2e8c
Yurlov <Benedictum1@mail.ru>
2017-07-06 16:56:32 +0300

    Update Application.java

--- src/main/java/online/mega/library/Application.java
+++ src/main/java/online/mega/library/Application.java
@@ -18,15 +18,15 @@ public class Application {
     }
 
 
-  @Bean
-    public CommandLineRunner demo(final UserService userService) {
-    return new CommandLineRunner() {
-        @Override
-        public void run(String... strings) throws Exception {
-           userService.addUser(new User("admin", "bcb049247ce6fab013d6f085b165e7c081ab93bd", UserRole.ADMIN));
-           userService.addUser(new User("user", "bcb049247ce6fab013d6f085b165e7c081ab93bd", UserRole.USER));
-        }
-    };
-}
+//   @Bean
+//     public CommandLineRunner demo(final UserService userService) {
+//     return new CommandLineRunner() {
+//         @Override
+//         public void run(String... strings) throws Exception {
+//            userService.addUser(new User("admin", "bcb049247ce6fab013d6f085b165e7c081ab93bd", UserRole.ADMIN));
+//            userService.addUser(new User("user", "bcb049247ce6fab013d6f085b165e7c081ab93bd", UserRole.USER));
+//         }
+//     };
+// }
 }
 
