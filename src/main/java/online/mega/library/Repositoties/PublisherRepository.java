99205ec3bf468e1d0d43ec198fbf02b2582789a0
Yurlov <Benedictum1@mail.ru>
2017-07-06 17:53:23 +0300

    Update PublisherRepository.java

--- src/main/java/online/mega/library/Repositoties/PublisherRepository.java
+++ src/main/java/online/mega/library/Repositoties/PublisherRepository.java
@@ -8,5 +8,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PublisherRepository extends JpaRepository<Publisher,Long> {
-
+    @Query("SELECT u FROM Publisher u where u.name = :name")
+    Publisher getPublisherByName(@Param("name") String name);
 }
