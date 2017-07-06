99477d5ae14ee0c11b8418cb618c5e6a55d06a61
Yurlov <Benedictum1@mail.ru>
2017-07-06 18:09:07 +0300

    Update Application.java

--- src/main/java/online/mega/library/Application.java
+++ src/main/java/online/mega/library/Application.java
@@ -14,11 +14,13 @@ public class Application {
 
 
 @Bean
-public CommandLineRunner demo() {
+public CommandLineRunner demo(final UserService userService) {
     return new CommandLineRunner() {
         @Override
         public void run(String... strings) throws Exception {
-            MyUtils.fillApp();
+            userService.addUser(new User("admin", "bcb049247ce6fab013d6f085b165e7c081ab93bd", UserRole.ADMIN));
+            userService.addUser(new User("user", "bcb049247ce6fab013d6f085b165e7c081ab93bd", UserRole.USER));
+            
         }
     };
 }
